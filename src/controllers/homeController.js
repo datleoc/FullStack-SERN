11ebafde9039dfa61f87545d79fc36f5eb636d8a
Steven Le <uslb98@gmail.com>
2023-07-17 22:42:58 +0700

    fifth commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -29,9 +29,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------------------')
-    console.log(data)
-    console.log('---------------------------------')
     return res.render('displayCRUD.ejs', {
         dataTable: data
     })
@@ -63,6 +60,16 @@ let putCRUD = async (req, res) => {
     });
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send("Deleted successfully!")
+    }
+    else {
+        return res.send("User not found!")
+    }
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
@@ -70,5 +77,6 @@ module.exports = {
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
